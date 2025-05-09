66150aa13b3793bd7213d7dfbee8044e39a55ef8
胡玮文 <huweiwen.hww@alibaba-inc.com>
2025-05-10 02:35:07 +0800

    fix build
    Add -DCMAKE_PREFIX_PATH to ensure the deps built by us is used.
    
    -DCXX_STD=c++20 may be override by CMAKE_CXX_STANDARD in old version of cmake. So just patch folly as upstream.

--- super/build.sh
+++ super/build.sh
@@ -2,6 +2,7 @@ export CC=clang-14
 export CXX=clang++-14
 export CFLAGS="-msse4.2 -mavx2"
 export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=lld"
 export CMAKE_BUILD_TYPE=RelWithDebInfo
 cmake -S . -B build -GNinja
 cmake --build build
